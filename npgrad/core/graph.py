0f2d63c48d181874f925fe5dd8e7bb9d0e0d16cf
cospectrum <severinalexeyv@gmail.com>
2023-03-23 21:30:17 +0300

    Change type of topological order: node indexes (in graph) instead of nodes

--- npgrad/core/graph.py
+++ npgrad/core/graph.py
@@ -1,5 +1,3 @@
-from __future__ import annotations
-
 from dataclasses import dataclass
 from typing import Generic, Sequence, TypeVar
 
@@ -8,6 +6,8 @@ from .node import Node, TokenId, TokenIds, T as Token
 
 Nodes = Sequence[Node[Token]]
 TokenMap = dict[TokenId, Token]
+NodeIndex = int
+Order = list[NodeIndex]
 
 T = TypeVar('T')
 
@@ -20,37 +20,41 @@ class Graph(Generic[T]):
         order = self.topological_order()
         return self.run_with_order(inputs, order)
 
-    def run_with_order(self, inputs: TokenMap, order: Nodes[T]) -> TokenMap[T]:
+    def run_with_order(self, inputs: TokenMap, order: Order) -> TokenMap[T]:
         sym_table = inputs.copy()
+        ordered_nodes = (self.nodes[index] for index in order)
 
-        for node in order:
+        for node in ordered_nodes:
             args = [sym_table[token_id] for token_id in node.inputs]
             out = node.eval(args)
             for token_id in node.outputs:
                 sym_table[token_id] = out
         return sym_table
 
-    def topological_order(self) -> Nodes[T]:
+    def topological_order(self) -> Order:
         order = []
         visited = set()
 
-        def visit(node: Node[T]) -> None:
-            if id(node) in visited:
+        def visit(node: Node[T], index: NodeIndex) -> None:
+            if index in visited:
                 return
-            visited.add(id(node))
-            children = self.children(node)
-            for child in children:
-                visit(child)
-            order.append(node)
+            visited.add(index)
+            for child_index in self.children_indexes(node):
+                child = self.nodes[child_index]
+                visit(child, child_index)
+            order.append(index)
 
-        for node in self.nodes:
-            visit(node)
+        for index, node in enumerate(self.nodes):
+            visit(node, index)
         return order
 
-    def children(self, node: Node[T]) -> Nodes[T]:
+    def children_indexes(self, node: Node[T]) -> list[NodeIndex]:
         inputs = node.inputs
 
         def wired(ids: TokenIds) -> bool:
             return len(set(ids).intersection(inputs)) != 0
 
-        return [n for n in self.nodes if wired(n.outputs)]
+        return [
+            index for index, other_node in enumerate(self.nodes)
+            if wired(other_node.outputs)
+        ]
